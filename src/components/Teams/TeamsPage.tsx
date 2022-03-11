8eb3b203239a17f50b6a488e7bdd6d22ffb5acdf
difalcosimon <61333124+simondifalco13@users.noreply.github.com>
2022-03-11 16:34:47 +0100

    Installing npm package for communication

---
+++ src/components/Teams/TeamsPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { GroupCallLocator, TeamsMeetingLinkLocator } from '@azure/communication-calling';
+import { CommunicationUserIdentifier } from '@azure/communication-common';
+
+
+const TeamPage = () =>{
+
+    return(
+        <>
+        </>
+    );
+}
+
+export default TeamPage;
